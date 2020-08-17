be114bbfcffc5b449d3fe48a08630c1525a0d323
zheng.zhao <zheng.zhao@supwisdom.com>
2020-08-17 18:30:53 +0800

    project init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,85 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="bg"></div>
+    <div class="container-box">
+      <div class="header">
+        <img src="../assets/images/logo.png">
+      </div>
+      <div class="authList">
+        <div class="authListLeft">
+          <img src="../assets/images/tu.png" >
+        </div>
+        <div class="authListRight">
+          <div class="newAuth">
+            <h2>新版认证</h2>
+            <ul>
+              <li v-for="(item,index) in list.new" :key="index"></li>
+              <li>迎新系统</li>
+              <li>学工系统</li>
+              <li>办公自动化</li>
+              <li>教务系统</li>
+              <li>研究生事物系统</li>
+              <li>网络教学平台</li>
+              <li>数据中心平台</li>
+              <li>校园一卡通</li>
+              <li>考勤系统</li>
+              <li>学生综合查询</li>
+            </ul>
+            <div class="clearfix"></div>
+          </div>
+          <div class="oldAuth">
+            <h2>老版认证</h2>
+            <ul>
+              <li v-for="(item,index) in list.old" :key="index"></li>
+              <li>密码维护</li>
+              <li>教务系统</li>
+              <li>办公自动化</li>
+              <li>教务系统</li>
+              <li>研究生事物系统</li>
+              <li>网络教学平台</li>
+              <li>数据中心平台</li>
+              <li>校园一卡通</li>
+              <li>考勤系统</li>
+              <li>学生综合查询</li>
+              <li>密码维护</li>
+              <li>教务系统</li>
+              <li>办公自动化</li>
+              <li>教务系统</li>
+              <li>研究生事物系统</li>
+              <li>网络教学平台</li>
+              <li>数据中心平台</li>
+              <li>校园一卡通</li>
+              <li>考勤系统</li>
+              <li>学生综合查询</li>
+            </ul>
+            <div class="clearfix"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <span>CopyRight</span>
+      <span>©2015-2016</span>
+      <span>版权所有</span>
+      <span>上海立信会计金融学院</span>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
+
+<style lang="scss">
+@import "../assets/css/index.css";
+img {
+  border: none;
+  vertical-align: middle;
+}
+.newAuth{
+  display: inline-block;
+}
+</style>
